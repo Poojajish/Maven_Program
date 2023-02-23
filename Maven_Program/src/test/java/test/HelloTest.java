860b18e73c4a46af27a675889b6289dd2afd8f62
Poojajishnu <mailtopoojasr@gmail.com>
2023-02-23 13:26:45 +0530

    my second commit

--- Maven_Program/src/test/java/test/HelloTest.java
+++ Maven_Program/src/test/java/test/HelloTest.java
@@ -4,7 +4,9 @@ public class HelloTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hello Test ");
+System.out.println("hello ");
+
+
 	}
 
 }
